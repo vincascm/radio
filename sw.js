4d71cac085bee3f1a9b0385415b71e46315a7893
vinoca <vinoca@vinoca.org>
2026-06-18 21:59:14 +0800

    feat: add PWA support with manifest, service worker, and app icon
    - manifest.json for installable web app
    - sw.js for offline shell caching
    - icon.svg as vector app icon
    - PWA meta tags and service worker registration in index.html

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE = 'radio-cache-v1';
+
+var PRECACHE_URLS = [
+  '/',
+  '/index.html',
+  '/radio.m3u8',
+  '/icon.svg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(PRECACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.map(function(key) {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      var fetchPromise = fetch(event.request).then(function(response) {
+        if (response && response.ok && response.type === 'basic') {
+          var clone = response.clone();
+          caches.open(CACHE).then(function(cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      }).catch(function() {
+        return cached;
+      });
+      return cached || fetchPromise;
+    })
+  );
+});
